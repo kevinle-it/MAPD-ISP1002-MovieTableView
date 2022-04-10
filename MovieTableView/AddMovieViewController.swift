e44cde84bd3775b6cdc574af3bc1fd8c61bdeb0d
Tri Le <trile95.it@gmail.com>
2022-04-10 00:09:44 -0400

    add new view controller class for add movie scene

---
+++ MovieTableView/AddMovieViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddMovieViewController.swift
+//  MovieTableView
+//
+//  Created by Tri Le on 4/10/22.
+//
+
+import UIKit
+
+class AddMovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
